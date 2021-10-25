619cd9bd65fdd26673a813c26565aba0407ff919
pjayathissa <p.jayathissa@gmail.com>
2021-10-25 13:22:22 +1300

    button selector working

--- js/score.js
+++ js/score.js
@@ -1,17 +1,34 @@
-const letters = ['p','b','t','d', 'k']
-console.log(letters)
+const letters = ["p", "b", "t", "d", "k"];
+console.log(letters);
 
+// This number will itterate through the letters
+currentLetterPosition = 0
 
-
-
-var Results = letters.map(value => {
+var Results = letters.map((value) => {
 	return {
-			letter: value,
-			begining: null,
-			middle: null,
-			end: null
-	}
+		letter: value,
+		begining: null,
+		middle: null,
+		end: null,
+	};
+});
 
-})
+function prepareButtons() {
+	console.log(Results);
 
-console.log(Results)
+	// Select all radios based on vlass .radio-btn
+	var radios = document.querySelectorAll(".radio-btn");
+
+	// itterate through each class, and create a function that fires when clicked
+	radios.forEach((radio) => {
+		radio.onclick = function(){
+			console.log(radio.getAttribute("value"))
+			Results[currentLetterPosition][radio.getAttribute("name")] = radio.getAttribute("value")
+			// if (radio.getAttribute("name") == "start-buttons") {
+			// 	Results[currentLetterPosition][radio.getAttribute("name")] = radio.getAttribute("value")
+			// }
+			console.log(Results)
+		}
+	});
+
+}
