c7f24951a385818286844eaca996b4672fe888e2
pjayathissa <p.jayathissa@gmail.com>
2021-10-30 14:58:44 +1300

    gettign ready to merge new html

--- js/main.js
+++ js/main.js
@@ -1,22 +1,24 @@
-// Add new letters as needed
-const letters = [
-	"P",
-	"B",
-	"T",
-	"D",
-	"K",
-	"G",
-	"F",
-	"V",
-	"M",
-	"N",
-	"L",
-	"H",
-	"S",
-	"Z",
-	"Sh",
-	"Ch",
-];
+/* 
+
+About: 
+Main .js file that manipulates the functionality of index.html
+
+Requirements:
+lettersLibrary.js contains the following constants
+- Letters: An Array of letters that will be renders
+- wordsLibrary: An object that maps letters to words and youtube links
+
+Notes:
+
+Developers:
+Jasper Hawkins
+Prageeth Jayathissa
+
+Date:
+November 2021
+
+*/
+
 
 // This number will itterate through the letters
 currentLetterPosition = 0;
@@ -31,74 +33,6 @@ var Results = letters.map((letter) => {
 		sound: null,
 	};
 });
-
-// Changing the wordLibrary below the images to match
-const wordLibrary = {
-	P: {
-		words: ["Pen", "Apple", "Cup"],
-		videoLink: "https://www.youtube.com/embed/VOENA0wtBOk",
-	},
-	B: {
-		words: ["Ball", "Baby", "Web"],
-		videoLink: "https://www.youtube.com/embed/VOENA0wtBOk",
-	},
-	T: {
-		words: ["Tent", "Butter", "Boat"],
-		videoLink: "https://www.youtube.com/embed/VOENA0wtBOk",
-	},
-	D: {
-		words: ["Duck", "Ladder", "Hand"],
-		videoLink: "https://www.youtube.com/embed/VOENA0wtBOk",
-	},
-	K: {
-		words: ["Cat", "Bucket", "Book"],
-		videoLink: "https://www.youtube.com/embed/VOENA0wtBOk",
-	},
-	G: {
-		words: ["Gate", "Tiger", "Dog"],
-		videoLink: "https://www.youtube.com/embed/VOENA0wtBOk",
-	},
-	F: {
-		words: ["Fire", "Dolphin", "Leaf"],
-		videoLink: "https://www.youtube.com/embed/VOENA0wtBOk",
-	},
-	V: {
-		words: ["Van", "TV", "Love"],
-		videoLink: "https://www.youtube.com/embed/VOENA0wtBOk",
-	},
-	M: {
-		words: ["Monkey", "Hammer", "Worm"],
-		videoLink: "https://www.youtube.com/embed/VOENA0wtBOk",
-	},
-	N: {
-		words: ["Nose", "Money", "Lion"],
-		videoLink: "https://www.youtube.com/embed/VOENA0wtBOk",
-	},
-	L: {
-		words: ["Lemon", "Toilet", "Girl"],
-		videoLink: "https://www.youtube.com/embed/VOENA0wtBOk",
-	},
-	H: {
-		words: ["Hat", "Beehive", ""],
-		videoLink: "https://www.youtube.com/embed/VOENA0wtBOk",
-	},
-	S: {
-		words: ["Sun", "Muscle", "House"],
-		videoLink: "https://www.youtube.com/embed/VOENA0wtBOk",
-	},
-	Z: {
-		words: ["Zebra", "Puzzle", "Cheese"],
-		videoLink: "https://www.youtube.com/embed/VOENA0wtBOk",
-	},
-	Sh: {
-		words: ["Shoe", "Fishing", "Bush"],
-		videoLink: "https://www.youtube.com/embed/VOENA0wtBOk",
-	},
-	Ch: {
-		words: ["Chair", "Catching", "Witch"],
-		videoLink: "https://www.youtube.com/embed/VOENA0wtBOk",
-	},
-};
 
 function initiatePage() {
 	// hide results
